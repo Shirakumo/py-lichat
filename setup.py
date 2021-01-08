d8556fc21e99159544ca9ff2ac12e82565983cf2
Shinmera <shinmera@tymoon.eu>
2021-01-08 23:49:28 +0100

    Set up the bare minimum of nothingness.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pylichat',
+    version='0.0.1',
+    description='A library for the Lichat protocol',
+    license='zlib',
+    packages=['pylichat'],
+    author='Benjamin Gerfelder',
+    author_email='shinmera@tymoon.eu',
+    keywords=['chat','protocol'],
+    url='https://github.com/shirakumo/py-lichat'
+)
