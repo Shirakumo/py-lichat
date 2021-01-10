6aa60cd0555c4257ffe2a7ef46cd58e781501ed6
Shinmera <shinmera@tymoon.eu>
2021-01-10 18:20:20 +0100

    Minor changes and some docs.

--- pylichat/client.py
+++ pylichat/client.py
@@ -7,6 +7,12 @@ import socket
 import base64
 
 class ConnectionFailed(Exception):
+    """Exception thrown when the connection attempt to the server fails for some reason.
+
+    May hold an update field with the Update instance that caused
+    the connection failure, if the server was reachable, but
+    connection failed for another reason.
+    """
     def __init__(self, update=None, message='Connection failed.'):
         self.update = update
         if update and get(update, 'message'):
@@ -14,6 +20,15 @@ class ConnectionFailed(Exception):
         super().__init__(message)
 
 class Channel:
+    """Representation of a channel the client is in.
+
+    Channels have a name, a list of users, and a dictionary
+    of channel information that is stored on servers with the
+    shirakumo-channel-info extension.
+
+    You may access the channel information by accessing the
+    channel object like a dictionary.
+    """
     def __init__(self, name):
         self.name = name
         self.users = set()
@@ -33,14 +48,43 @@ class Channel:
         return value
 
 class Emote:
+    """Representation of an emote the server sent back.
+
+    An emote has a name, a content_type (as a mime-type string)
+    and a binary payload that describes the actual image data
+    """
     def __init__(self, name, content_type, payload):
         self.name = name
         self.content_type = content_type
         self.payload = payload
     
 class Client:
-    def __init__(self, username=None):
+    """A basic Lichat client using TCP sockets.
+
+    The client has a username, password, servername, a
+    connected flag, supperted extensions list, channels
+    dictionary, and emotes dictionary.
+    
+    The client will take care of basic connection
+    establishment and maintenance of information.
+    All you need to do is actually display and access
+    the information present in the client, and sent
+    through updates.
+
+    In order to add a handler function for an update,
+    you may add a handler using add_handler.
+    
+    Updates can be sent out with send, received
+    with recv, and handled with handle.
+
+    When constructed the client will not attempt
+    connection. Connection has to be manually
+    initiated with the connect function.
+    """
+    
+    def __init__(self, username=None, password=None):
         self.username = username
+        self.password = password
         self.servername = None
         self.connected = False
         self.extensions = []
@@ -104,23 +148,54 @@ class Client:
         }
 
     def add_handler(self, update, fun):
+        """Adds a handler function for the given update type.
+
+        Supplied to the handler function are the client
+        and the update objects. add_handler expects an Update
+        class as argument for which the handler function
+        is invoked. Note that only exact type matches are
+        dispatched, the class hierarchy is ignored. You
+        may however install a handler on Update, which
+        will be invoked for every update, regardless of
+        actual type.
+        """
         if update not in self.handlers:
             self.handlers[update] = []
         self.handlers[update].append(fun)
 
     def clock():
+        """Returns the current time in universal-time"""
         int(time.time()) + 2208988800
 
     def next_id(self):
+        """Returns a new unique ID."""
         self.id = self.id+1
         return self.id
 
     def is_supported(self, extension):
+        """Returns true if the given extension is supported by client and server."""
         return extension in self.extensions
 
-    def connect(self, host, port=1111, timeout=10.0):
+    def connect(self, host, port=1111, timeout=10.0, username=None, password=None):
+        """Attempts to establish a connection to the given server.
+
+        You may also pass in the username and password,
+        as well as a timout for the connection attempt.
+        This will perform a full connection handshake.
+        After this you should start processing updates
+        immediately.
+
+        If you want to receive the Connect update, you
+        must install a handler for it prior to calling
+        this function.
+        
+        Should the connection attempt fail for whatever
+        reason, a ConnectionFailed exception is thrown.
+        """
+        if username != None: self.username = username
+        if password != None: self.password = password
         self.connect_raw(host, port)
-        self.send(update.Connect, version=update.version, extensions=update.extensions)
+        self.send(update.Connect, password=password, version=update.version, extensions=update.extensions)
         updates = self.recv(timeout)
         if updates:
             if type(updates[0]) is not update.Connect:
@@ -130,7 +205,15 @@ class Client:
         else:
             raise ConnectionFailed(message="Timeout")
 
+    def disconnect(self):
+        """Initiates a disconnect handshake if the client is connected."""
+        if self.connected:
+            self.send(update.Disconnect)
+
     def send(self, type, **args):
+        """Sends a new update for the given type and set of arguments.
+
+        See make_instance"""
         args['from'] = self.username
         args['clock'] = Client.clock()
         args['id'] = self.next_id()
@@ -139,6 +222,13 @@ class Client:
         return instance.id
 
     def recv(self, timeout=0):
+        """Receive updates.
+
+        Returns a list of update instances that were received.
+        Timeout may be passed to specify a maximum amount of
+        time to wait for updates. Note that this function may
+        return even if there are more updates ready to be read.
+        """
         strings = self.recv_raw(timeout)
         updates = []
         for string in strings:
@@ -148,13 +238,17 @@ class Client:
         return updates
 
     def handle(self, instance):
-        handlers = self.handlers.get(instance.__class__, None)
-        if handlers == None:
-            handlers = self.handlers[update.Update]
-        for handler in handlers:
+        """Handle the given update instance.
+
+        This delivers it to the various handler functions.
+        """
+        for handler in self.handlers.get(instance.__class__, []):
+            handler(self, instance)
+        for handler in self.handlers[update.Update]:
             handler(self, instance)
 
     def loop(self):
+        """Perform a basic connection loop of just receiving and handling updates."""
         while self.connected:
             for update in self.recv(1.0):
                 self.handle(update)
@@ -200,6 +294,13 @@ class Client:
         return parts
 
 def read_update(string, i=0):
+    """Parse an update from the string.
+
+    Returns it and the position in the string after the update.
+
+    If no update can be parsed or the item is not an update,
+    None is returned instead of the update
+    """
     (dat, i) = wire.from_string(string, i)
     if type(dat) == list and 0 < len(dat):
         instance = update.make_instance_plist(dat[0], dat[1:])
